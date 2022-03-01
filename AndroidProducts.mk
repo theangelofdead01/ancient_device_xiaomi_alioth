39ad18e0ebc52efe9982309f04225ad23d143f0e
theangelofdead01 <rockinthecity142@gmail.com>
2022-03-01 14:46:48 -0500

    Alioth: AncientOS-fy

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 AncientOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/nad_alioth.mk
+    $(LOCAL_DIR)/ancient_alioth.mk
 
 COMMON_LUNCH_CHOICES := \
-    nad_alioth-user \
-    nad_alioth-userdebug \
-    nad_alioth-eng
+    ancient_alioth-user \
+    ancient_alioth-userdebug \
+    ancient_alioth-eng
--- ancient_alioth.mk
+++ ancient_alioth.mk
@@ -8,20 +8,32 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+# Inherit some common Aosp stuff.
+$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-TARGET_BOOT_ANIMATION_RES := 108o
-PRODUCT_NAME := nad_alioth
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := ancient_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO F3
 
-TARGET_SUPPORTS_QUICK_TAP := true
+# Gapps
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_STOCK_ARCORE := true
+
+# Inherit some AncientOS stuff.
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_BOOT_ANIMATION_RES := 1080
+EXTRA_FOD_ANIMATIONS := true
+ANCIENT_OFFICIAL := true
+ANCIENT_GAPPS := true
+ANCIENT_WEEABO := true
+FORCE_LAWNCHAIR=true
+#TARGET_SUPPORTS_QUICK_TAP := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
